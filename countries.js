9d0a493e7aa9cb768156eb8bccecfe15544e9332
panuwatat <panuwatat@gmail.com>
2025-03-02 12:01:55 +0700

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,8 @@
+const countries = [
+    { code: "thailand", name: "Thailand", flag: "flags/thailand.png" },
+    { code: "usa", name: "USA", flag: "flags/usa.png" },
+    { code: "japan", name: "Japan", flag: "flags/japan.png" },
+    // เพิ่มประเทศอื่นๆ ตามต้องการ
+];
+
+export default countries;
